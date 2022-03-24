3d0434c790378a813e9e63f89ca39ad7be7a68ad
zbx <2694735157@qq.com>
2022-03-24 15:49:31 +0800

    通用异常处理,响应处理

---
+++ auth/src/main/java/com/zbx/auth/AuthApplication.java
@@ -0,0 +1,19 @@
+package com.zbx.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * @时间 2022/3/24
+ * @作者 zbx
+ * @描述
+ **/
+@SpringBootApplication
+public class AuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthApplication.class,args);
+    }
+
+}
